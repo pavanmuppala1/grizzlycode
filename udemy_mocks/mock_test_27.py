8792edb3cc8e62e4b395f56726f5e09ccd7b9178
pavanmuppala1 <pavanmuppala2002@gmail.com>
2024-03-09 12:29:52 +0530

    this is my third part of udemy mock

---
+++ udemy_mocks/mock_test_27.py
@@ -0,0 +1,33 @@
+num = int(input('give a number: '))
+if num > 0:
+    if num < 5:
+        print('1')
+    else:
+        print('2')
+else:
+        if num < -1:
+                print('3')
+        else:
+                print('4')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
